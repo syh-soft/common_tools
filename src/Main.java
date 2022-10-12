7460be714aeb9037c47fe5f2bafe404f7478b50a
孙雨寒 <1293174521@qq.com>
2022-10-12 23:03:49 +0800

    删除了部分不需要的内容

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import com.syh.example.A;
+
 
 import java.util.Arrays;
 import java.util.TreeSet;
@@ -7,19 +7,10 @@ import java.util.Scanner;
 import java.util.*;
 
 public class Main {
-    public static boolean isIsomorphic( List<Integer> items) {
-     System.out.println(items.stream().filter(item->(item-1>0&&item-2<5)).count());
-     return true;
-    }
+
 
     public static void main(String[] agrs) {
-        List<Integer> items = new ArrayList<>();
 
-        items.add(5);
-        items.add(6);
-        items.add(7);
-        items.add(8);
-        isIsomorphic(items);
 
     }
 
